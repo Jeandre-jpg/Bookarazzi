d3798f1949bd6b862a77cb555f356c896a74cf75
Jeandré De Villiers <190025@virtualwindow.co.za>
2021-09-16 12:21:58 +0200

    Initial Commit

---
+++ Bookarazzi/BookarazziApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookarazziApp.swift
+//  Bookarazzi
+//
+//  Created by Jeandré De Villiers on 2021/09/16.
+//
+
+import SwiftUI
+
+@main
+struct BookarazziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookarazzi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookarazzi
+//
+//  Created by Jeandré De Villiers on 2021/09/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
